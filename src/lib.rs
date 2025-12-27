a84f5ec466aa4292d6ecad6f4bb40d05e877eecd
omelet1 <omelet1@omelet1s-MacBook-Air.local>
2025-12-27 15:14:47 +0900

    main.rs lib.rs 완벽 분리

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 pub mod timer;
 pub mod settings;
 
+use std::time::Duration;
+
+use dioxus::prelude::*;
+
+use timer::components::Timer;
+use timer::logic::use_timer;
+use settings::components::SettingsButton;
+
+#[component]
+pub fn DioxusTimerDisplay() -> Element {
+    /*
+       initial_duration과 SettingsUI를 연결
+    */
+    let initial_duration = Duration::from_secs(10);
+    let (timer, tx) = use_timer(initial_duration);
+    rsx! {
+        div {
+            class: "dioxus-timer-display",
+            Timer {timer, tx}
+            SettingsButton {}
+        }
+
+    }
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use dioxus::prelude::*;
 
-use std::time::Duration;
-
-// use dioxus_timer::settings::components;
-use dioxus_timer::timer::components::Timer;
-use dioxus_timer::timer::logic::use_timer;
-use dioxus_timer::settings::components::SettingsButton;
+use dioxus_timer::DioxusTimerDisplay;
 
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 
@@ -25,26 +20,3 @@ fn App() -> Element {
     }
 }
 
-#[component]
-fn DioxusTimerDisplay() -> Element {
-    /*
-       initial_duration과 SettingsUI를 연결
-    */
-    let initial_duration = Duration::from_secs(10);
-    let (timer, tx) = use_timer(initial_duration);
-    rsx! {
-        div {
-            class: "dioxus-timer-display",
-            Timer {timer, tx}
-            SettingsButton {}
-        }
-
-    }
-}
-
-// type TimerValue = (Signal<DioxusTimer>, Coroutine<DioxusTimerCommand>);
-
-// timer는 signal과 코루틴을 튜플로 반환해야 한다
-
-// 설정 버튼뿐만 아니라 설정 화면도 만들어야 한다.
-// Settings UI
